a2e203347b9c8aa87f4d98a1fb2f4ee9a87b993c
Joseph <josephtecson18@gmail.com>
2021-09-01 17:44:36 -0700

    add changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import PersonCard from "./components/MyNewComponent"
 function App() {
   return (
     <div className="App">
-      <PersonCard name="Doe, Jane" age="Age: {this.state.age}" hair_color="Hair Color: Black" />
-      <PersonCard name="Smith, John" age="Age: {this.state.age}" hair_color="Hair Color: Brown" />
+      <PersonCard name="Doe, Jane" age={45} hair_color="Hair Color: Black" />
+      <PersonCard name="Smith, John" age={88} hair_color="Hair Color: Brown" />
     </div>
   );
 }
--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,39 +1,33 @@
     import React, { Component } from "react";
 
     class PersonCard extends Component {
-    constructor(props) {
-    super(props);
-    // State is anything that can change.
-    this.state = {
-        age: 45,
-        age: 88
-    };
+    
+        constructor(props) {
+            super(props);
+            // State is anything that can change.
+    
+            this.state = {
+                age: this.props.age,
+            };
     }
 
-    handleCountAge = (event) => {
-        this.setState({
-            age: this.state.age + 1
-        })
-
+    handleCountAge = () => {
+        this.setState({ age: this.state.age + 1});
     };
 
     render() {
-    return (
-        <div>
-        <h2>{this.props.name}</h2>
-        <p>{this.state.age}</p>
-        <p>{this.props.hair_color}</p>
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>Age: {this.state.age}</p>
+                <p>{this.props.hair_color}</p>
 
-        <button
-            onClick={(event) => {
-            this.handleCountAge(event);
-            }}
-        >
-            Birthday Button for {this.props.name}
-        </button>
-        </div>
-    );
+                <button onClick= {this.handleCountAge}>
+                    Birthday Button for {this.props.name}
+                </button>
+            </div>
+        );
     }
-    }
+}
 
     export default PersonCard;
